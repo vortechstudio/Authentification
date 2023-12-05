78f8553a067ec261f410f9b333d9b2d4045fbcb2
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2023-12-05 10:14:56 +0100

    Update captcha implementation in form
    Switched from `{{ captcha_img() }}` to `{!! captcha_img() !!}` to display captcha image. Also removed `w-25` CSS class from the captcha input field for better responsiveness. These changes ensure proper rendering of captcha image and enhance user experience on various screen sizes.

--- resources/views/components/form/captcha.blade.php
+++ resources/views/components/form/captcha.blade.php
@@ -1,4 +1,4 @@
 <div class="mb-10">
-    {{ captcha_img() }}
-    <input type="text" name="captcha" class="form-control w-25 bg-white" placeholder="Tapez les caractères de validation" required>
+    {!! captcha_img() !!}
+    <input type="text" name="captcha" class="form-control bg-white" placeholder="Tapez les caractères de validation" required>
 </div>
