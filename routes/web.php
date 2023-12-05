e53924da7f579a8532459d96c063d828a388823d
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2023-12-05 09:47:10 +0100

    The root URL now redirects to the login page instead of displaying the welcome view. This change is aimed to ensure that only authenticated users are allowed access to the main application, which significantly improves security and user management.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', \App\Livewire\Auth\Login::class)->name('login');
