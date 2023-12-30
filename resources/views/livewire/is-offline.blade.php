c61f5d7e831bbeaf4fb0011ff4135c5c12b13f1f
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2023-12-30 23:05:51 +0100

    ✨ feat: add IsOffline Livewire component, push_subscriptions migration, and webpush config
    - Add IsOffline Livewire component
    - Add push_subscriptions migration
    - Add webpush config
    - All files are encoded in UTF-8

---
+++ resources/views/livewire/is-offline.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="rounded-3 bg-white w-75 p-5 h-250px">
+        <div class="d-flex flex-column justify-content-center align-items-center">
+            <i class="fa-solid fa-power-off text-danger fs-3tx"></i>
+            <span class="fs-2x fw-bold">Système Hors Ligne</span>
+            <span>Vous êtes actuellement hors ligne !</span>
+        </div>
+    </div>
+</div>
