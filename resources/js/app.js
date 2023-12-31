b59840c8d180681bdc217c63591c8b86420215ec
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2023-12-31 16:01:07 +0100

    🔄 fix: remove unnecessary import.meta.glob
    Remove unnecessary import.meta.glob in app.js
    
    Co-authored-by: Maxim <maxim@vortechstudio.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,6 @@ import "../../public/plugins/custom/iconify/iconify.min.js";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap-select/dist/js/bootstrap-select.js";
 
-import.meta.glob([
-    '../media/**',
-])
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
