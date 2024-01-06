019efc703c4ca0f218e5e3ac0a46ff7138c9eb5f
Mockelyn Maxime <mmockelyn@vortechstudio.fr>
2024-01-06 17:55:03 +0100

    :sparkles: feat(Events): Ajouter un nouveau fichier ModelCreated.php

---
+++ app/Events/ModelCreated.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ModelCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct()
+    {
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new PresenceChannel('channel-name')
+        ];
+    }
+}
